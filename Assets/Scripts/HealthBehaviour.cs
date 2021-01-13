668d0f462163653668df6abc4f2f3748991b5a45
GehadAbdElhalim <halimgego@gmail.com>
2021-01-14 00:16:59 +0200

    Added health script and added a function for spawning the turrets

---
+++ Assets/Scripts/HealthBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthBehaviour : MonoBehaviour
+{
+    public static UnityEvent OnPlayerDead;
+    public float maxHealth;
+    float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Damage(float amount)
+    {
+        if(currentHealth <= 0)
+        {
+            return;
+        }
+
+        currentHealth -= maxHealth;
+
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            OnPlayerDead.Invoke();
+        }
+    }
+}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,9 +7,13 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] Vector2 cellSize = new Vector2(1, 1);
     [SerializeField] Vector2Int mapSize;
     [SerializeField] GameObject cell;
+    [SerializeField] GameObject turret;
+
+    public Vector3[,] cellPositions;
 
     private void Start()
     {
+        cellPositions = new Vector3[mapSize.x, mapSize.y];
         GenerateMap(mapSize.x, mapSize.y, cellSize.x, cellSize.y);
     }
 
@@ -26,14 +30,20 @@ public class MapGenerator : MonoBehaviour
 
                 GameObject go = Instantiate(cell, new Vector3(x, transform.position.y, z), Quaternion.identity, transform);
                 go.transform.localScale = new Vector3(cellHeight, 1, cellWidth);
+
+                cellPositions[i, j] = new Vector3(x, transform.position.y, z);
             }
         }
 
         gameObject.AddComponent<BoxCollider>().size = new Vector3(mapHeight * cellHeight, 0.1f, mapWidth * cellWidth);
     }
 
-    bool IsOdd(float value)
+    void SpawnTurrets(int[] rows, int[] cols)
     {
-        return value % 2 == 1;
+        for(int i = 0; i < rows.Length; i++)
+        {
+            GameObject go = Instantiate(turret, cellPositions[rows[i], cols[i]], Quaternion.identity);
+            go.transform.Rotate(Vector3.up, Random.Range(0f, 360f));
+        }
     }
 }
